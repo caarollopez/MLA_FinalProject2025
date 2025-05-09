338db0027982b7c2e1b795b2a7f595896878826f
JaimeSalafranca10 <jaimesalafranca23@gmail.com>
2025-05-09 10:06:06 +0200

    solving github

---
+++ solve_github.py
@@ -0,0 +1,14 @@
+import nbformat
+
+notebook_path = "Fake_News_Detection.ipynb"  # Cambia este nombre
+
+nb = nbformat.read(open(notebook_path, "r", encoding="utf-8"), as_version=4)
+
+nb.metadata.pop("widgets", None)
+
+for cell in nb.cells:
+    cell.metadata.pop("widgets", None)
+
+nbformat.write(nb, open(notebook_path, "w", encoding="utf-8"))
+
+print("Cleaned notebook saved (overwritten)")
